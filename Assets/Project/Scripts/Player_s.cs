7362999e7069120b3c314126256bf0342470d39b
calvemoe <thephonep@gmail.com>
2018-09-19 14:39:11 +0300

    passed 5 lessons

---
+++ Assets/Project/Scripts/Player_s.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_s : MonoBehaviour {
+
+    //player parameters
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+	}
+}
