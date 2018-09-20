bad2e810c25d118bbd48d2c13defba4e2815b344
calvemoe <thephonep@gmail.com>
2018-09-21 00:02:36 +0300

    pass 3 lessons

--- Assets/Project/Scripts/Player_s.cs
+++ Assets/Project/Scripts/Player_s.cs
@@ -14,18 +14,54 @@ public class Player_s : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        
+        //if keyboard/joystick input?
         if (Input.GetAxis("Horizontal") < 0)
         {
+            //moving left by keyboard/joystick
             GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
         }
         else if (Input.GetAxis("Horizontal") > 0)
         {
+            //moving right by keyboard/joystick
             GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
         }
         else
         {
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            //if mouse/touch input
+            if (Input.GetMouseButton(0))
+            {
+                Vector2 relativeMousePosition = new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
+
+                if (relativeMousePosition.x < 0.5f)
+                {
+                    //moving left by mouse/touch
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+                }
+                else if (relativeMousePosition.x > 0.5f)
+                {
+                    //moving right by mouse/touch
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+                }
+            }
+            else
+            {
+                //stay still
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            }
         }
-	}
+
+        //keep player within bounce
+        float spriteWidth = GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        float screenLimit = (Camera.main.orthographicSize * Camera.main.aspect) - spriteWidth / 2;
+        if (transform.position.x > screenLimit)
+        {
+            transform.position = new Vector3(screenLimit, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -screenLimit)
+        {
+            transform.position = new Vector3(-screenLimit, transform.position.y, transform.position.z);
+
+        }
+    }
 }
---
+++ Assets/Project/Scripts/SceneManager3DSpace1.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SceneManager3DSpace1 : MonoBehaviour {
+
+    public Text infoText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        infoText.text = "No input detected";
+
+        List<Vector2> touchCoordinates = new List<Vector2>();
+
+        //actual device touches
+        foreach(Touch touch in Input.touches)
+        {
+            touchCoordinates.Add(touch.position);
+        }
+
+        //dummy touches
+       if (Input.GetMouseButton(0))
+        {
+            touchCoordinates.Add(Input.mousePosition);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            touchCoordinates.Add(new Vector2(42, 42));
+        }
+
+        if (Input.GetKey(KeyCode.V))
+        {
+            touchCoordinates.Add(new Vector2(48, 48));
+        }
+ 
+        if(touchCoordinates.Count > 0)
+        {
+            infoText.text = "";
+            for (int i = 0; i < touchCoordinates.Count; i++)
+            {
+                infoText.text += string.Format("Input {0}: {1}, {2}\n", i + 1, touchCoordinates[i].x, touchCoordinates[i].y);
+            }
+        }
+    }
+}
