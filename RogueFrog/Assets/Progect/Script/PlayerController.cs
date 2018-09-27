571f0df86bdc673f5c32a7c348d33a19906b56ce
calvemoe <thephonep@gmail.com>
2018-09-27 16:01:09 +0300

    switch project

---
+++ RogueFrog/Assets/Progect/Script/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        if (horizontalMovement > 0)
+        {
+            transform.position = new Vector3(transform.position.x + 0.32f, transform.position.y, transform.position.z);
+        }
+        else if (horizontalMovement < 0)
+        {
+            transform.position = new Vector3(transform.position.x - 0.32f, transform.position.y, transform.position.z);
+        }
+        if (verticalMovement > 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.32f, transform.position.z);
+        }
+        else if (verticalMovement < 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.32f, transform.position.z);
+        }
+
+    }
+}
