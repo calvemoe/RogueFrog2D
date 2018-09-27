4ad50e76912a9352a8248892871f82853641b125
calve <thephonep@gmail.com>
2018-09-27 20:08:12 +0300

    pass 2 lessons -> game scoring

--- RogueFrog/Assets/Progect/Script/PlayerController.cs
+++ RogueFrog/Assets/Progect/Script/PlayerController.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float jumpDistance = 0.32f;
+
+    private bool jumped;
+
+    private Vector3 startingPosition;
+
 	// Use this for initialization
 	void Start () {
-		
+        startingPosition = transform.position;
 	}
 	
 	// Update is called once per frame
@@ -14,22 +20,71 @@ public class PlayerController : MonoBehaviour {
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
 
-        if (horizontalMovement > 0)
+        Vector2 targetPosition = Vector2.zero;
+
+        bool tryToMove = false;
+
+        //Movement logic
+        if (!jumped)
         {
-            transform.position = new Vector3(transform.position.x + 0.32f, transform.position.y, transform.position.z);
+
+            if (horizontalMovement > 0)
+            {
+                targetPosition = new Vector2(transform.position.x + jumpDistance, transform.position.y);
+                tryToMove = true;
+            }
+            else if (horizontalMovement < 0)
+            {
+                targetPosition = new Vector2(transform.position.x - jumpDistance, transform.position.y);
+                tryToMove = true;
+            }
+            else if (verticalMovement > 0)
+            {
+                targetPosition = new Vector2(transform.position.x, transform.position.y + jumpDistance);
+                tryToMove = true;
+            }
+            else if (verticalMovement < 0)
+            {
+                targetPosition = new Vector2(transform.position.x, transform.position.y - jumpDistance);
+                tryToMove = true;
+            }
+
+            Collider2D hitCollider = Physics2D.OverlapCircle(targetPosition, 0.1f);
+            if (hitCollider == null && tryToMove == true)
+            {
+                jumped = true;
+                transform.position = targetPosition;
+            }
         }
-        else if (horizontalMovement < 0)
+        else
         {
-            transform.position = new Vector3(transform.position.x - 0.32f, transform.position.y, transform.position.z);
+            if (horizontalMovement == 0 && verticalMovement == 0)
+            {
+                jumped = false;
+            }
         }
-        if (verticalMovement > 0)
+
+        //keep player in bounds.
+        if (transform.position.y < -(Screen.height / 100f) / 2f)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + 0.32f, transform.position.z);
+            transform.position = new Vector2(transform.position.x, transform.position.y + jumpDistance);
         }
-        else if (verticalMovement < 0)
+
+        if (transform.position.x < -(Screen.width / 100f) / 2f)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - 0.32f, transform.position.z);
+            transform.position = new Vector2(transform.position.x + jumpDistance, transform.position.y);
         }
+
+        if (transform.position.x > (Screen.width / 100f) / 2f)
+        {
+            transform.position = new Vector2(transform.position.x - jumpDistance, transform.position.y);
+        }
+
+        if (transform.position.y > (Screen.height / 100f) / 2f)
+        {
+            transform.position = startingPosition;
+        }
+
 
     }
 }
