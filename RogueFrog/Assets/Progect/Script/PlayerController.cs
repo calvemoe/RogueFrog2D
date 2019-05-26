e4da7b0534ab41fcefdd08f645b9f2ce2d3313b6
calvemoe <thephonep@gmail.com>
2019-05-26 22:52:57 +0300

    refactoring for optimization purposes added tag to enemy objects and changed triggering in this way

--- RogueFrog/Assets/Progect/Script/PlayerController.cs
+++ RogueFrog/Assets/Progect/Script/PlayerController.cs
@@ -15,10 +15,17 @@ public class PlayerController : MonoBehaviour {
 
     private bool jumped;
     private Vector3 startingPosition;
- 
+    private float boundaryH;
+    private float boundaryW;
+
+    private Vector2 targetPosition;
+
 	// Use this for initialization
-	void Start () {
+	void Awake() {
         startingPosition = transform.position;
+        boundaryH = (Screen.height / 100f) / 2f;
+        boundaryW = (Screen.width / 100f) / 2f - 0.32f;
+        targetPosition = Vector2.zero;
 	}
 
     // Update is called once per frame
@@ -26,86 +33,62 @@ public class PlayerController : MonoBehaviour {
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
 
-        Vector2 targetPosition = Vector2.zero;
-
         bool tryToMove = false;
 
         //Movement logic
-        if (!jumped)
-        {
+        if (!jumped) {
 
-            if (horizontalMovement > 0)
-            {
+            if (horizontalMovement > 0) {
                 targetPosition = new Vector2(transform.position.x + jumpDistance, transform.position.y);
                 tryToMove = true;
             }
-            else if (horizontalMovement < 0)
-            {
+            else if (horizontalMovement < 0) {
                 targetPosition = new Vector2(transform.position.x - jumpDistance, transform.position.y);
                 tryToMove = true;
             }
-            else if (verticalMovement > 0)
-            {
+            else if (verticalMovement > 0) {
                 targetPosition = new Vector2(transform.position.x, transform.position.y + jumpDistance);
                 tryToMove = true;
             }
-            else if (verticalMovement < 0)
-            {
+            else if (verticalMovement < 0) {
                 targetPosition = new Vector2(transform.position.x, transform.position.y - jumpDistance);
                 tryToMove = true;
             }
 
             Collider2D hitCollider = Physics2D.OverlapCircle(targetPosition, 0.05f);
-            if ((hitCollider == null || hitCollider.GetComponent<Enemy>() != null) && tryToMove == true)
-            {
+            if ((hitCollider == null || hitCollider.CompareTag("Enemy")) && tryToMove == true) {
                 GetComponent<AudioSource>().Play();
                 jumped = true;
                 transform.position = targetPosition;
                 if (OnPlayerMoved != null)
-                {
                     OnPlayerMoved();
-                }
             }
         }
-        else
-        {
+        else {
             if (horizontalMovement == 0 && verticalMovement == 0)
-            {
                 jumped = false;
-            }
         }
 
         //keep player in bounds.
-        if (transform.position.y < -(Screen.height / 100f) / 2f)
-        {
+        if (transform.position.y < -boundaryH)
             transform.position = new Vector2(transform.position.x, transform.position.y + jumpDistance);
-        }
 
-        if (transform.position.x < -(Screen.width / 100f) / 2f)
-        {
+        if (transform.position.x < -boundaryW)
             transform.position = new Vector2(transform.position.x + jumpDistance, transform.position.y);
-        }
 
-        if (transform.position.x > (Screen.width / 100f) / 2f)
-        {
+        if (transform.position.x > boundaryW)
             transform.position = new Vector2(transform.position.x - jumpDistance, transform.position.y);
-        }
 
-        if (transform.position.y > (Screen.height / 100f) / 2f)
-        {
+        //if top position was reached
+        if (transform.position.y > boundaryH) {
             transform.position = startingPosition;
             if (OnPlayerEscaped != null)
-            {
                 OnPlayerEscaped();
-            }
         }
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.GetComponent<Enemy>() != null)
-        {
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Enemy"))
             Destroy(gameObject);
-        }
     }
 }
